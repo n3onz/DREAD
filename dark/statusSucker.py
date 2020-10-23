557ff471bd8f1f32c7029356b7c1cd2c3d70dc56
Aaron Mullally <46582416+aaron-mullally@users.noreply.github.com>
2020-10-23 10:15:28 -0400

    Update Status Sucker
    Now gets time of update, status code, and title of website.

--- dark/statusSucker.py
+++ dark/statusSucker.py
@@ -1,5 +1,7 @@
 import requests
 import sys
+from bs4 import BeautifulSoup
+import datetime
 
 websites = [x.strip('\n') for x in open(sys.argv[1],'r').read().split(',')]
 
@@ -7,11 +9,14 @@ products = []
 
 for site in websites:
     r = requests.get(site)
+    t = r.text
+    soup = BeautifulSoup(t, features = "html.parser")
+    title = soup.title.string
     sc = r.status_code
-    products.append('{}:{}'.format(site,sc))
+    dt = datetime.datetime.now()
+    products.append('{}:{}:{}:{}'.format(site,sc,title,dt))
 
 out = ','.join(products)+'\n'
 
 with open('statusCodes.csv', 'w') as f:
     f.write(out)
-
