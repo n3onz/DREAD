14c3615a7d8fcae76e392caf55c27f17b99f894f
Aaron Mullally <46582416+aaron-mullally@users.noreply.github.com>
2020-10-26 15:48:47 -0400

    Added Text Tokenization w/ spaCy

--- dark/statusSucker.py
+++ dark/statusSucker.py
@@ -3,6 +3,9 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 import datetime
+from spacy.lang.en import English
+from spacy.lang.en.stop_words import STOP_WORDS
+nlp = English()
 
 #pulls in the list of websites from CSV
 websites = [x.strip('\n') for x in open(sys.argv[1],'r').read().split(',')]
@@ -14,8 +17,16 @@ for site in websites:
     r = requests.get(site)
     t = r.text
     soup = BeautifulSoup(t, features = "html.parser")
-    title = soup.title.string
+    title = soup.title.string #Gets title from HTML
+    #Gets text from HTML and tokenizes text removing stop words, punctuation, white-space, and duplicate words
     page_text = soup.get_text()
+    doc = nlp(page_text)
+    token_list = []
+    for token in doc:
+        if (token.is_ascii == True and token.is_stop == False and token.is_punct == False and token.is_space == False):
+            if (token.text not in token_list):
+                token_list.append(token.text)
+    #Gets status code and datetime
     sc = r.status_code
     dt = datetime.datetime.now()
     products.append('{}:{}:{}:{}\n'.format(site,sc,title,dt))
