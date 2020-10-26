a0a75985fa5ee1ca35a5794978bcdbfff3634eb9
Aaron Mullally <46582416+aaron-mullally@users.noreply.github.com>
2020-10-26 11:27:13 -0400

    Updated script to include scheduling
    Currently set to run at 0600 every day, checks for time every 1 minute

--- parser/ccupdater.py
+++ parser/ccupdater.py
@@ -1,42 +1,55 @@
+import os
+import schedule
+import time
 import requests
 from bs4 import BeautifulSoup
 
-site = 'https://commoncrawl.org/the-data/get-started/'
+def updater():
+    site = 'https://commoncrawl.org/the-data/get-started/'
 
-r = requests.get(site)
-t = r.text
-soup = BeautifulSoup(t, features = 'html.parser')
+    r = requests.get(site)
+    t = r.text
+    soup = BeautifulSoup(t, features = 'html.parser')
+    
+    
+    counter = 0
+    new_link_spot = 0
+    new_link = ''
+    
+    for link in soup.find_all('a'):
+        if (link.get('href') == 'https://commoncrawl.s3.amazonaws.com/'):
+            new_link_spot = counter - 1
+        counter += 1
+    
+    new_link = soup.find_all('a')[new_link_spot]
+    new_link = new_link.get('href')
+    
+    updated = 1
+    with open('ccLink.txt', 'r') as f:
+        if (f.read() != new_link):
+            updated = 0
+    
+    with open('ccLink.txt', 'w') as f:
+        f.write(new_link)
+        if (updated == 0):
+            print('Updated CC link!')
+        else:
+            print('Link was already up to date!')
+    
+    soup2 = BeautifulSoup(requests.get(new_link).text, features = 'html.parser')
+    
+    dl_link = ''
+    for link in soup2.find_all('a'):
+        if 'wet.paths.gz' in link.get('href'):
+            dl_link = link.get('href')
+    
+    download = 'wget ' + dl_link
+    print(download)
+    os.system(download)
 
+schedule.every().day.at("06:00").do(updater)
 
-counter = 0
-new_link_spot = 0
-new_link = ''
+while True:
+    schedule.run_pending()
+    time.sleep(60)
 
-for link in soup.find_all('a'):
-    if (link.get('href') == 'https://commoncrawl.s3.amazonaws.com/'):
-        new_link_spot = counter - 1
-    counter += 1
-
-new_link = soup.find_all('a')[new_link_spot]
-new_link = new_link.get('href')
-
-updated = 1
-with open('ccLink.txt', 'r') as f:
-    if (f.read() != new_link):
-        updated = 0
-
-with open('ccLink.txt', 'w') as f:
-    f.write(new_link)
-    if (updated == 0):
-        print('Updated CC link!')
-    else:
-        print('Link was already up to date!')
-
-soup2 = BeautifulSoup(requests.get(new_link).text, features = 'html.parser')
-
-dl_link = ''
-for link in soup2.find_all('a'):
-    if 'wet.paths.gz' in link.get('href'):
-        dl_link = link.get('href')
-
-print(dl_link)
